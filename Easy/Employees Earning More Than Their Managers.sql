75c6ddea0292261423a88e5cfb51ce08d019dae5
Anshu Kushwah <110227518+AnshuK23@users.noreply.github.com>
2023-05-01 10:07:32 +0530

    Update Employees Earning More Than Their Managers.sql

--- Easy/Employees Earning More Than Their Managers.sql
+++ Easy/Employees Earning More Than Their Managers.sql
@@ -1,22 +1,22 @@
-PROBLEM : 181
+-- PROBLEM : 181
 
-Table: Employee
+-- Table: Employee
 
-+-------------+---------+
-| Column Name | Type    |
-+-------------+---------+
-| id          | int     |
-| name        | varchar |
-| salary      | int     |
-| managerId   | int     |
-+-------------+---------+
-id is the primary key column for this table.
-Each row of this table indicates the ID of an employee, their name, salary, and the ID of their manager.
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- | salary      | int     |
+-- | managerId   | int     |
+-- +-------------+---------+
+-- id is the primary key column for this table.
+-- Each row of this table indicates the ID of an employee, their name, salary, and the ID of their manager.
 
-Write an SQL query to find the employees who earn more than their managers.
-Return the result table in any order.
+-- Write an SQL query to find the employees who earn more than their managers.
+-- Return the result table in any order.
 
-SOLUTION : 
+-- SOLUTION : 
 select E1.name as Employee
 from Employee as E1 left join  Employee as E2 
 on E1.managerId = E2.id
