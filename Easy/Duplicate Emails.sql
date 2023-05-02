b2b942b130717591864c570219f387fe7b423012
Anshu Kushwah <110227518+AnshuK23@users.noreply.github.com>
2023-05-02 19:32:26 +0530

    Update Duplicate Emails.sql

--- Easy/Duplicate Emails.sql
+++ Easy/Duplicate Emails.sql
@@ -1,20 +1,20 @@
-PROBLEM:182
+-- PROBLEM:182
 
-Table: Person
+-- Table: Person
 
-+-------------+---------+
-| Column Name | Type    |
-+-------------+---------+
-| id          | int     |
-| email       | varchar |
-+-------------+---------+
-id is the primary key column for this table.
-Each row of this table contains an email. The emails will not contain uppercase letters.
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | email       | varchar |
+-- +-------------+---------+
+-- id is the primary key column for this table.
+-- Each row of this table contains an email. The emails will not contain uppercase letters.
  
 
-Write an SQL query to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
+-- Write an SQL query to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
 
-SOLUTION:
+-- SOLUTION:
 SELECT email
 FROM Person
 GROUP BY email
