5cdf29fd526c1c82fbf3738bb3fc1c852d56db3d
Anshu Kushwah <110227518+AnshuK23@users.noreply.github.com>
2023-05-03 11:15:04 +0530

    Update Customers Who Never Order.sql

--- Easy/Customers Who Never Order.sql
+++ Easy/Customers Who Never Order.sql
@@ -1,34 +1,34 @@
-PROBLEM:183
+-- PROBLEM:183
 
-Table: Customers
+-- Table: Customers
 
-+-------------+---------+
-| Column Name | Type    |
-+-------------+---------+
-| id          | int     |
-| name        | varchar |
-+-------------+---------+
-id is the primary key column for this table.
-Each row of this table indicates the ID and name of a customer.
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | name        | varchar |
+-- +-------------+---------+
+-- id is the primary key column for this table.
+-- Each row of this table indicates the ID and name of a customer.
 
-Table: Orders
+-- Table: Orders
 
-+-------------+------+
-| Column Name | Type |
-+-------------+------+
-| id          | int  |
-| customerId  | int  |
-+-------------+------+
-id is the primary key column for this table.
-customerId is a foreign key of the ID from the Customers table.
-Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | id          | int  |
+-- | customerId  | int  |
+-- +-------------+------+
+-- id is the primary key column for this table.
+-- customerId is a foreign key of the ID from the Customers table.
+-- Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
  
 
-Write an SQL query to report all customers who never order anything.
+-- Write an SQL query to report all customers who never order anything.
 
-Return the result table in any order.
+-- Return the result table in any order.
 
-SOLUTION:
+-- SOLUTION:
 
 SELECT Customers.name AS Customers
 FROM Customers FULL JOIN Orders ON Customers.id = Orders.customerId 
