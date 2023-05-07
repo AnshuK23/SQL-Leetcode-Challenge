8373c025dd2df05143e38a194326b1f5f18f8f60
Anshu Kushwah <110227518+AnshuK23@users.noreply.github.com>
2023-05-07 21:43:37 +0530

    Create Rising Temperature.sql

---
+++ Easy/Rising Temperature.sql
@@ -0,0 +1,24 @@
+PROBLEM : 197
+
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the primary key for this table.
+This table contains information about the temperature on a certain day.
+ 
+
+Write an SQL query to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+SOLUTION:
+
+SELECT w1.id 
+FROM Weather w1 ,Weather w2 
+WHERE w1.temperature < w2.temperature AND DATEDIFF(day,w1.recordDate,w2.recordDate) =1;
